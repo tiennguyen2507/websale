df387e7f82ec37ee88a8a8ff5e81cb1830581a73
tien nguyen <nguyenledinhtien2507>
2021-07-09 15:24:05 +0700

    commit

---
+++ js/data.js
@@ -0,0 +1,45 @@
+var data= [
+    {id:'1',
+    category: "phone",
+    nameSP: " Apple iphone 5",
+    price: 3000000,
+    rate:5,
+    title:"apple giá rẻ chính hãng",
+    image:"image/SP/iphone5.jpg"
+
+    },
+    {id:'2',
+    category: "phone",
+    nameSP: " Apple iphone 6",
+    price: 4000000,
+    rate:5,
+    title:"apple giá rẻ chính hãng",
+    image:"image/SP/iphone6.jpg"
+    },
+    {id:'3',
+    category: "phone",
+    nameSP: " Apple iphone 7",
+    price: 5000000,
+    rate:5,
+    title:"apple giá rẻ chính hãng",
+    image:"image/SP/iphone7.jpg"
+    },
+    {id:'4',
+    category:"phone",
+    nameSP: " Apple iphone x",
+    price: 7000000,
+    rate:5,
+    title:"apple giá rẻ chính hãng",
+    image:"image/SP/iphonex.jpg"
+    },
+    {id:'5',
+    category:"houseware",
+    nameSP: " Bàn là hơi nước",
+    price: 1000000,
+    rate:5,
+    title:"bàn là chính hảng panasonic",
+    image:"image/SP/banla.jpg"
+    }
+]
+ var local = localStorage.setItem('data',JSON.stringify(data))
+ 
--- js/index.js
+++ js/index.js
@@ -8,12 +8,31 @@ function slideimage(){
         
     }
     document.getElementById('slideimage').src = slide[index]
-    console.log(index)
-
-    
-    
 }
 setInterval(slideimage,2000)
 
 
+ var localdata=localStorage.getItem('data')
+   data = JSON.parse(localdata)
+   console.log(data[1].category)
+
+//  var selectSP = document.getElementById('selectSP').options[e.selectedIndex].value
+//  console.log(selectSP)
+function selectSP(obj){
+    var value =obj.value
+    console.log(value)
+    var show01=[]
+    var show=[]
+    for (let i = 0; i < data.length; i++) {
+        if (value == data[i].category) {
+            show.push(data[i].nameSP)
+        }
+    }
+    var show01 =show.map(value=>{
+      return  `<li>
+               <a href=""> ${value}</a>
+              </li>`
+    })
+    document.getElementById("showSP").innerHTML = show01
+}
  
