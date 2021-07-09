2c0be28e2a53b3ee297da23b9c8839179ff11a36
tien nguyen <nguyenledinhtien2507>
2021-07-09 21:50:30 +0700

    dsdsa

---
+++ js/addSP.js
@@ -0,0 +1,21 @@
+
+var addcategory =['abc']
+function addselect(obj){
+    addcategory.unshift(obj.value)
+    addcategory.pop()
+    console.log(addcategory)
+}
+ 
+function addSP(){
+    
+    
+       
+    var addname = document.getElementById("addtenSP").value;
+    var addgiaSP =document.getElementById("addgiaSP").value
+    var addrate = document.getElementById("addrate").value
+    var addtitle = document.getElementById("addtitle").value
+    var addimage = document.getElementById("addimage").value
+    var addcategory =""
+    console.log(addname)
+    console.log(addcategory)
+}
